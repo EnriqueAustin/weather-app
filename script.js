8bf015a03124179445ff2c5e96171d5f5ea1c596
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-05-05 03:34:18 +0200

    created script.js & connected to & populated by API

---
+++ script.js
@@ -0,0 +1,41 @@
+// Should be in .env file ... lazy
+const API_KEY = "005c528eedc3c8deec0b157a26be8a78";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    // console.log(cityValue);
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}&units=metric`);
+
+        if (!response.ok){
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        // console.log(data);
+
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}`,
+            `Wind speed: ${data.wind.speed}`,
+        ];
+
+    } catch (error) {
+        
+    }
+}
